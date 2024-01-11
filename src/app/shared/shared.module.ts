985f68da4018b5af60acf11a6a676ebb825a876f
Alicia Zamorano <azpans@hotmail.com>
2024-01-11 15:55:40 +0100

    navbar-footer routing working

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NavbarFooterComponent } from './components/navbar-footer/navbar-footer.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
 })
 export class SharedModule { }
